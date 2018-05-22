edcadfdb8c73343734dcc293e1d6f1114285d3d9
CodeHunger <info@codehunger.com>
2018-05-22 21:51:52 +0200

    Added mass assihgnment fields

--- app/Key.php
+++ app/Key.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\User;
 
 class Key extends Model{
-	public function user() {
-        return $this->belongsTo(User::class);
-    }    
+	protected $fillable = [
+        'hash', 'active'
+    ];    
 }
